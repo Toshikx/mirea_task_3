ef0d68d235d56357baae2147bf47131255650ed6
Toshik <isaev.antonio@gmail.com>
2020-09-17 22:06:56 +0300

    Practice3&4

---
+++ src/ru/mirea/ikbo_16_19/isaev/Human.java
@@ -0,0 +1,65 @@
+package ru.mirea.ikbo_16_19.isaev;
+
+import java.util.Arrays;
+
+public class Human {
+    Leg[] legs = {
+            new Leg("Left leg"), new Leg("Right leg")
+    };
+    Hand[] hands = {
+            new Hand("Left hand"), new Hand("Right hand")
+    };
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "legs=" + Arrays.toString(legs) +
+                ", hands=" + Arrays.toString(hands) +
+                '}';
+    }
+}
+
+class Leg extends Human {
+    String legPlace;
+
+    public Leg(String legPlace) {
+        this.legPlace = legPlace;
+    }
+
+    @Override
+    public String toString() {
+        return "Leg{" +
+                "legPlace='" + legPlace + '\'' +
+                '}';
+    }
+}
+
+class Hand extends Human{
+    String handPlace;
+
+    public Hand(String handPlace) {
+        this.handPlace = handPlace;
+    }
+
+    @Override
+    public String toString() {
+        return "Hand{" +
+                "handPlace='" + handPlace + '\'' +
+                '}';
+    }
+}
+
+class Head extends Human{
+    String takeAnotherHead;
+
+    public Head(String takeAnotherHead) {
+        this.takeAnotherHead = takeAnotherHead;
+    }
+
+    @Override
+    public String toString() {
+        return "Head{" +
+                "takeAnotherHead='" + takeAnotherHead + '\'' +
+                '}';
+    }
+}
---
+++ src/ru/mirea/ikbo_16_19/isaev/TestMain.java
@@ -0,0 +1,11 @@
+package ru.mirea.ikbo_16_19.isaev;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Circle cir = new Circle(12, 0, 0);
+        System.out.println(cir.toString());
+        Human chelovecheck = new Human();
+        System.out.println(chelovecheck.toString());
+        System.out.println("Думаю это стоит оценить на 5");
+    }
+}
